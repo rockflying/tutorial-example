ce333917e5bf86d1e6f7cebd89b966096f83a2d9
Li Li <joeyli1989@gmail.com>
2014-04-08 22:23:40 +0200

    Update script.sh

--- script/script.sh
+++ script/script.sh
@@ -19,3 +19,44 @@ do
 		fi
 	fi
 done
+
+
+
+#! /usr/bin/python
+import sys
+import os
+import shutil
+import zipfile
+
+srcDir=sys.argv[1]
+destDir=sys.argv[2]
+minSize=sys.argv[3]
+maxSize=sys.argv[4]
+
+if minSize == "0":
+	minSize = 0
+
+count = 1 
+
+def traverseDir( path ):
+	global count
+	for (dirpath, dirnames, filenames) in os.walk(path):
+		for filename in filenames:
+			filepath = dirpath + "/" + filename
+			if not zipfile.is_zipfile(filepath):
+				continue
+			if count >= 1000:
+				sys.exit(0)
+			destFilepath = destDir + "/{}.apk".format(count);
+			zf = zipfile.ZipFile(filepath, 'r')
+			size = zf.getinfo('classes.dex').file_size
+			zf.close()
+			if int(size) <= int(maxSize):
+				print str(size) + ":" + str(maxSize)
+				if int(size) >= int(minSize):
+					shutil.copy2(filepath, destFilepath)
+					count = count + 1
+	return
+
+
+traverseDir( srcDir )
